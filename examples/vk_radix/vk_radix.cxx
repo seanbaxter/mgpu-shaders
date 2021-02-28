906c9d7c40f1603f6abe96f2003ecafb89fa92e8
Sean Baxter <lightborn@gmail.com>
2021-02-28 00:26:00 -0500

    Removed unused code

--- examples/vk_radix/vk_radix.cxx
+++ examples/vk_radix/vk_radix.cxx
@@ -7,60 +7,6 @@
 using namespace mgpu;
 using namespace mgpu::vk;
 
-// Return a throughput in elements/second.
-template<typename type_t, int nt, int vt>
-double benchmark_radix(context_t& context, int count, int num_iterations,
-  const type_t* init) {
-
-  // Allocate test data storage.
-  type_t* gpu = context.alloc_gpu<type_t>(count);
-
-  // Create a command buffer.
-  cmd_buffer_t cmd_buffer(context);
-  cmd_buffer.begin();
-
-  // Copy in host data.
-  cmd_buffer.memcpy(gpu, init, sizeof(type_t) * count);
-
-  // Query the radix sort for auxiliary data size and allocate.
-  void* aux_data = nullptr;
-  size_t aux_size = 0;
-  radix_sort<nt, vt, 4>(aux_data, aux_size, cmd_buffer, gpu, count);
-
-  // Do one run to let the shader compiler work.
-  aux_data = context.alloc_gpu(aux_size);
-  radix_sort<nt, vt, 4>(aux_data, aux_size, cmd_buffer, gpu, count);
-  
-  // End and submit the command buffer.
-  cmd_buffer.end();
-  context.submit(cmd_buffer);
-  cmd_buffer.reset();
-
-  // And wait for it to be done.
-  vkQueueWaitIdle(context.queue);
-
-  timespec start;
-  clock_gettime(CLOCK_REALTIME, &start);
-  
-  cmd_buffer.begin();
-  for(int i = 0; i < num_iterations; ++i)
-    radix_sort<nt, vt, 8>(aux_data, aux_size, cmd_buffer, gpu, count);
-
-  cmd_buffer.end();
-  context.submit(cmd_buffer);
-  vkQueueWaitIdle(context.queue);
-
-  timespec end;
-  clock_gettime(CLOCK_REALTIME, &end);
-
-  double elapsed = (end.tv_sec - start.tv_sec) + 
-    (end.tv_nsec - start.tv_nsec) * 1.0e-9;
-  context.free(aux_data);
-  context.free(gpu);
-
-  return elapsed;
-}
-
 int main() {
   context_t context;
 
